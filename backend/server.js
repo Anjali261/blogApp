f0050a25c426e3f4d8f9a95f25e2dab1f47e6b28
Anjali261 <mail2anjali.singh2610@gmail.com>
2024-04-09 15:20:11 +0530

    Adding Middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,22 @@
-import app from "./app.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+const app = express();
 
-app.listen(4000,() =>{
-    console.log('Server is listening at port ${4000}')
+dotenv.config({path : "./config/config.env"})
+export default app;
+
+app.use(cors({
+    origin:[],
+    methods:["GET","POST", "PUT","DELETE"],
+    credentials:true
+}));
+
+app.use(cookieParser());
+app.use(express.json()); 
+app.use(express.urlencoded({extended:true}));
+
+app.listen(process.env.PORT, () =>{
+    console.log(`Server is listening at http://localhost:${process.env.PORT}`);
 })
